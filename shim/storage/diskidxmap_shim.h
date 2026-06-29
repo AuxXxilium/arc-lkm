cd335e5cbb13bafa1e048f59540fddba139d32ed
AuxXxilium <info@auxxxilium.tech>
2026-06-29 18:57:48 +0200

    tree: add idx mapping
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

---
+++ shim/storage/diskidxmap_shim.h
@@ -0,0 +1,7 @@
+#ifndef REDPILL_DISKIDXMAP_SHIM_H
+#define REDPILL_DISKIDXMAP_SHIM_H
+
+int register_diskidxmap_shim(void);
+int unregister_diskidxmap_shim(void);
+
+#endif /* REDPILL_DISKIDXMAP_SHIM_H */
